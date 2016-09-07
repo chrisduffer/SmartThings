663b531b6f8205f9887d242fa7d5e88a2ac0ce0a
Chris Evans <chrisduffer@gmail.com>
2016-09-07 10:01:07 +0100

    Single colour for current power

--- devicetypes/chrisduffer/eagle-owl.src/eagle-owl.groovy
+++ devicetypes/chrisduffer/eagle-owl.src/eagle-owl.groovy
@@ -18,28 +18,19 @@ metadata {
 		capability "Polling"
 		capability "Power Meter"
 		capability "Refresh"
-	}
-
+	}  
 
 	simulator {
 	}
     
-     preferences {
+    preferences {
     	input("ip", "string", title:"IP Address", description: "192.168.1.126", required: true, displayDuringSetup: true)
-    }
-
+	}
+    
 	tiles {
 		valueTile("power", "device.power", width: 2, height: 2) {
-			state "default", label:'${currentValue}', unit:"kW",
-            backgroundColors:[
-            [value: 0, color: "#153591"],
-            [value: 200, color: "#1e9cbb"],
-            [value: 400, color: "#90d2a7"],
-            [value: 600, color: "#44b621"],
-            [value: 800, color: "#f1d801"],
-            [value: 1000, color: "#d04e00"],
-            [value: 1200, color: "#bc2323"]
-        ]
+			state "default", label:'${currentValue} W', unit:"W",
+            backgroundColor: "#0066ff"
 		}
         
         valueTile("lastdata", "device.lastdata", width: 2, height: 2) {
